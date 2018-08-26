1879bed2ec6369d324991fb90cfee9e10427c590
em <ernesto.mudu@gmail.com>
2018-08-25 17:26:14 -0700

    Sample watchman-processor bitbar plugin.

---
+++ index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env /usr/local/bin/node
+
+process.env.PATH = '/usr/local/bin/';
+process.title = 'watchman-processor-bitbar-plugin';
+const { processor } = require('watchman-processor');
+const notifier = require('node-notifier');
+
+const watchman = processor;
+
+if (watchman && typeof watchman.start === 'function') {
+  watchman.emitter.on('error', function (params) {
+    notifier.notify('Message');
+   console.log(':skull:');
+  });
+  watchman.emitter.on('setState', function (params) {
+   switch(params.status) {
+     case 'good':
+       notifier.notify('👍');
+       console.log(':+1:');
+       break;
+     case 'running':
+       notifier.notify('🏃');
+       console.log(':running_man:');
+       break;
+     case 'error':
+       notifier.notify('💀');
+       console.log(':skull:');
+       break;
+   }
+  });
+  watchman.start()
+}
+process.on('SIGINT', function() {
+  watchman.end().then(function() {
+    process.exit();
+  });
+});
